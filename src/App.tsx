d11c2c41e43ca0e322850118ca8a165fc513cc8f
DmytroZozuliak <dmytro.zozuliak@ellorem.com>
2023-03-15 11:46:46 +0200

    feat: add prettier eslint, husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
-
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni minus ducimus quam earum
+      asperiores atque veritatis odio optio quidem. Doloremque eligendi at commodi dignissimos nihil
+      debitis repellendus mollitia similique quisquam.
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
