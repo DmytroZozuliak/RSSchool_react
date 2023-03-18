326e75873453852cce8dbccc7adb1aa2b7582e6e
DmytroZozuliak <dmytro.zozuliak@ellorem.com>
2023-03-18 12:39:32 +0200

    feat: 404 route

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,16 +1,18 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from 'react-router-dom';
 import Layout from '../components/layouts';
+import { ROUTE_PATHS } from '../constants/routePaths';
 import AboutPage from './AboutPage';
 import HomePage from './HomePage';
 import NotFoundPage from './NotFoundPage';
 
 export const routesFromElements = createRoutesFromElements(
   <>
-    <Route path="/" element={<Layout />}>
+    <Route path={ROUTE_PATHS.homePage} element={<Layout />}>
       <Route index element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
+      <Route path={ROUTE_PATHS.aboutPage} element={<AboutPage />} />
     </Route>
-    <Route path="*" element={<NotFoundPage />} />
+    <Route path={ROUTE_PATHS.notFoundPage} element={<NotFoundPage />} />
+    <Route path={ROUTE_PATHS.other} element={<Navigate to={ROUTE_PATHS.notFoundPage} />} />
   </>
 );
 
