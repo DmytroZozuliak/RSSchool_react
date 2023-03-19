440f1accfc44e2004d2f0b76f9bc2f2a643c9807
DmytroZozuliak <dmytro.zozuliak@ellorem.com>
2023-03-19 22:55:31 +0200

    feat: add base gh-pages to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/RSSchool_react/',
+  // base: '/',
 
   test: {
     globals: true,
