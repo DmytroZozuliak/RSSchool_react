7ee0d17ea40dad8338e856a126f12b63e157faaf
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2023-03-22 12:48:44 +0200

    refactor: MyCheckbox component to functional

--- src/components/UI/MyCheckbox/MyCheckbox.tsx
+++ src/components/UI/MyCheckbox/MyCheckbox.tsx
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
 import styles from './MyCheckbox.module.scss';
-
-interface Props {
+interface MyCheckboxProps {
   label: string;
   errorMessage?: string;
   reference?: React.RefObject<HTMLInputElement>;
   valid?: boolean;
 }
 
-export default class ToggleCheckbox extends Component<Props & React.HTMLProps<HTMLInputElement>> {
-  render() {
-    const { reference, valid, label, errorMessage, ...restProps } = this.props;
+const MyCheckbox = (props: MyCheckboxProps & React.HTMLProps<HTMLInputElement>) => {
+  const { reference, valid, label, errorMessage, ...restProps } = props;
 
-    let isValid = false;
-    if (!valid) {
-      isValid = true;
-    }
-
-    return (
-      <div className={styles.wrapper}>
-        <label htmlFor={label} className={`${styles.title} ${isValid ? styles.invalid : ''}`}>
-          {label}
-        </label>
-        <input className={styles.input} id={label} type="checkbox" ref={reference} {...restProps} />
-        <label className={styles.label} htmlFor={label} />
-
-        {isValid && <span>{errorMessage}</span>}
-      </div>
-    );
+  let isValid = false;
+  if (!valid) {
+    isValid = true;
   }
-}
+
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={label} className={`${styles.title} ${isValid ? styles.invalid : ''}`}>
+        {label}
+      </label>
+      <input className={styles.input} id={label} type="checkbox" ref={reference} {...restProps} />
+      <label className={styles.label} htmlFor={label} />
+
+      {isValid && <span>{errorMessage}</span>}
+    </div>
+  );
+};
+
+export default MyCheckbox;
