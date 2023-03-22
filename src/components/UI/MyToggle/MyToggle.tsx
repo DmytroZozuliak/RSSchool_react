6a4cf02c2520c1705bfdf945a7449fc415bf5ddd
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2023-03-22 12:59:35 +0200

    refactor: MyToggle component to functional

--- src/components/UI/MyToggle/MyToggle.tsx
+++ src/components/UI/MyToggle/MyToggle.tsx
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
 import styles from './MyToggle.module.scss';
-
-interface Props {
+interface ToggleCheckboxProps {
   label: string;
   errorMessage?: string;
   option1: string;
@@ -11,38 +9,38 @@ interface Props {
   valid?: boolean;
 }
 
-export default class ToggleCheckbox extends Component<Props & React.HTMLProps<HTMLInputElement>> {
-  render() {
-    const { reference1, reference2, option1, option2, valid, label, errorMessage, ...restArgs } =
-      this.props;
+const MyToggle = (props: ToggleCheckboxProps & React.HTMLProps<HTMLInputElement>) => {
+  const { reference1, reference2, option1, option2, valid, label, errorMessage, ...restArgs } =
+    props;
 
-    let isValid = false;
-    if (!valid) {
-      isValid = true;
-    }
-
-    return (
-      <div className={styles.toggleWrapper}>
-        <div className={styles.title}>{label}</div>
-        <div className={`${styles.switch} ${styles['switch--horizontal']}`}>
-          <input
-            id="radio-a"
-            type="radio"
-            value={option1}
-            defaultChecked
-            ref={reference1}
-            {...restArgs}
-          />
-          <label htmlFor="radio-a">{option1}</label>
-          <input id="radio-b" type="radio" value={option2} ref={reference2} {...restArgs} />
-          <label htmlFor="radio-b">{option2}</label>
-          <span className={styles['toggle-outside']}>
-            <span className={styles['toggle-inside']}></span>
-          </span>
-        </div>
-
-        {isValid && <span>{errorMessage}</span>}
-      </div>
-    );
+  let isValid = false;
+  if (!valid) {
+    isValid = true;
   }
-}
+
+  return (
+    <div className={styles.toggleWrapper}>
+      <div className={styles.title}>{label}</div>
+      <div className={`${styles.switch} ${styles['switch--horizontal']}`}>
+        <input
+          id="radio-a"
+          type="radio"
+          value={option1}
+          defaultChecked
+          ref={reference1}
+          {...restArgs}
+        />
+        <label htmlFor="radio-a">{option1}</label>
+        <input id="radio-b" type="radio" value={option2} ref={reference2} {...restArgs} />
+        <label htmlFor="radio-b">{option2}</label>
+        <span className={styles['toggle-outside']}>
+          <span className={styles['toggle-inside']}></span>
+        </span>
+      </div>
+
+      {isValid && <span>{errorMessage}</span>}
+    </div>
+  );
+};
+
+export default MyToggle;
