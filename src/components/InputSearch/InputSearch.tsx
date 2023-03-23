24f985503a33a8262410676b06ed5f8d026efdb4
DmytroZozuliak <dmytro.zozuliak@gmail.com>
2023-03-23 17:30:02 +0200

    feat: searchInput useEffect with useRef

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -1,18 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import MyInput from '../UI/MyInput';
 
 const InputSearch = () => {
   const [term, setTerm] = useState(() => localStorage.getItem('goodsSearchBar') || '');
+  const inputRefValue = useRef(term);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTerm(e.target.value);
+    inputRefValue.current = e.target.value;
   };
 
   useEffect(() => {
     return () => {
-      localStorage.setItem('goodsSearchBar', term);
+      localStorage.setItem('goodsSearchBar', inputRefValue.current);
     };
-  }, [term]);
+  }, []);
 
   return <MyInput type="search" placeholder="search..." value={term} onChange={onChangeHandler} />;
 };
